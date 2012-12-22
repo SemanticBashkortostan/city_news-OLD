1c104debb4fa5d7d29d0365f336b14552d0652a7
Idris <sld7700@gmail.com>
2012-12-23 00:37:50 +0600

    Added city regexp mapping to city name AND removed lemmatizing in naive bayes initializer.

--- config/initializers/naive_bayes.rb
+++ config/initializers/naive_bayes.rb
@@ -8,12 +8,11 @@ module NaiveBayes
       # (Ишим+[[:word:]]+|ИШИМ+[[:word:]]+|ишим+[[:word:]]+)
       regexp_ishimbay = /(Ишим+[[:word:]]+|ИШИМ+[[:word:]]+|ишим+[[:word:]]+)/   # Ишембай - bashkirian
 
-      regexp_salavat = /(Салав+[[:word:]]+|САЛАВ+[[:word:]]+|салав+[[:word:]]+)/
-      regexp_ufa = /(Уф+[[:word:]]+|УФ+[[:word:]]+|уфи+[[:word:]]+)/
-      regexp_str = /(Стерл+[[:word:]]+|СТЕРЛ+[[:word:]]+|стерл+[[:word:]]+)/
+      regexp_salavat = {:regexp => /(Салав+[[:word:]]+|САЛАВ+[[:word:]]+|салав+[[:word:]]+)/, :name => "Салават"}
+      regexp_ufa = {:regexp => /(Уф+[[:word:]]+|УФ+[[:word:]]+|уфи+[[:word:]]+)/, :name => "Уфа"}
+      regexp_str = {:regexp => /(Стерл+[[:word:]]+|СТЕРЛ+[[:word:]]+|стерл+[[:word:]]+)/, :name => "Стерлитамак"}
       regexp_domain = /Domain:.+/
- 	    features = [string.scan(regexp_str), string.scan(regexp_salavat), string.scan(regexp_ufa)].flatten.map{ |word| word.mb_chars.downcase.to_s }
-      features.map!{|e| lemmatize(e)}.compact!
+ 	    features = [scan(string, regexp_str), scan(string, regexp_salavat), scan(string, regexp_ufa)].compact.map{ |word| word.mb_chars.downcase.to_s }
       features << string.scan(regexp_domain)[0].split("/")[2]
       features
     end
@@ -21,6 +20,11 @@ module NaiveBayes
 
     def lemmatize word
       (`./lib/turglem-client #{word}`).split(" ")[1]
+    end
+
+
+    def scan string, regexp_hash
+      regexp_hash[:name] unless string.scan( regexp_hash[:regexp] ).empty?
     end
   end
 
