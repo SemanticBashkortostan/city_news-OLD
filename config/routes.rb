310eee7beed099fc5c08df47c39c8d4773aab52e
Idris <sld7700@gmail.com>
2013-02-22 00:16:53 +0600

    Added normal 404 page showing

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,12 @@ CityNews::Application.routes.draw do
   root :to => "feeds#index"
   devise_for :users
 
+  match '/goto' => 'feeds#goto', :as => 'feeds_goto'
   match ':city' => 'feeds#index', :as => 'feeds_city'
   match '/' => 'feeds#index', :as => 'feeds'
+
+  unless Rails.application.config.consider_all_requests_local
+    match '*not_found', to: 'application#render_404'
+  end
 
 end
