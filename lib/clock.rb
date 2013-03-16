0476a0488f6ebdaf38c1dee72349c01ad5522218
Idris <sld7700@gmail.com>
2013-03-16 16:29:32 +0600

    Updated clock.rb

--- lib/clock.rb
+++ lib/clock.rb
@@ -1,6 +1,5 @@
 require 'clockwork'
 
-require File.expand_path('../../config/boot',        __FILE__)
 require File.expand_path('../../config/environment', __FILE__)
 require "#{Rails.root}/lib/scheduler/classifier"
 require "#{Rails.root}/lib/scheduler/production_feeds_fetcher"
