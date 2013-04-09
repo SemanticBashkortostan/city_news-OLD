e843732682c564fc4f9e31361d2be57d44ec2632
Idris <sld7700@gmail.com>
2013-04-09 13:13:48 +0600

    Developing NbWithDict

--- lib/nb_with_dict.rb
+++ lib/nb_with_dict.rb
@@ -15,14 +15,19 @@ class NbWihDict
   end
 
 
-  def run
+  def run    
+    Feed.tagged_with(["dev_train", "to_train", "was_trainer"], :any => true).where( :text_class_id => TextClass.all  ).each do |feed|
+      @nb.train( feed.features_for_text_classifier, feed.text_class_id )
+    end
+
+
     pos_train_data = Feed.tagged_with(["dev_train", "to_train", "was_trainer"], :any => true).where( :text_class_id => pos_tc  )
     neg_train_data  = Feed.tagged_with(["dev_train", "to_train", "was_trainer"], :any => true).where( 
                                         :text_class_id => (cities - [pos_tc])  
                                         ).all[0..(pos_train_data.count*0.3).ceil] + outlier_data[0...outlier_data.count/2]
     train_data = pos_train_data + neg_train_data
 
-    test_data  = Feed.tagged_with("dev_test").where( :text_class_id => cities ).all + outlier_data[outlier_data.count/2...outlier_data.count]
+    test_data  = Feed.tagged_with("dev_test").where( :text_class_id => cities ).all
 
   end
 
