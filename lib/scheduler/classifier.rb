24f20e464aa7886582ddc3a523fc1afb433e9400
Idris <sld7700@gmail.com>
2013-04-26 13:51:19 +0600

    Extracted old classifying by MNB method from scheduler/classifier.rb. Also added new classifying by rose-mnb, but under development

--- lib/scheduler/classifier.rb
+++ lib/scheduler/classifier.rb
@@ -1,16 +1,21 @@
 class Scheduler::Classifier
   def self.classify_fetched
-    classifiers_ensemble = ClassifiersEnsemble.new( [Classifier.find_by_name("#{Classifier::NAIVE_BAYES_NAME}-all")], :preload => true )
-
+    ensbm_rose_mnb_classifiers = Classifier.where("name like '#{Classifier::ROSE_NAIVE_BAYES_NAME}%'").all.collect do |cl|
+      ClassifiersEnsemble.new( [cl], :preload => true )  
+    end
     Feed.unclassified_fetched.each do |feed|
-      classify_info = classifiers_ensemble.classify( feed.string_for_classifier )
-      tag_list = ["classified"]
-      if classify_info[:recommend_as_train] == true
-        tag_list << "to_train"
+      ensbm_rose_mnb_classifiers.each do |classifier|
+        classify_info = classifiers_ensemble.classify( feed )
+        #TODO: Make it to work for several classes. 
+        #So we may have to_train for several_classes and simply several text_classes. ? has_many :through ?
+        tag_list = ["classified"]
+        if classify_info[:recommend_as_train] == true
+          tag_list << "to_train"
+        end
+        feed.mark_list += tag_list
+        feed.text_class = TextClass.find classify_info[:class]
+        feed.save
       end
-      feed.mark_list += tag_list
-      feed.text_class = TextClass.find classify_info[:class]
-      feed.save
     end
   end
 
@@ -25,6 +30,21 @@ class Scheduler::Classifier
         classifier.train_feeds << train_feed
       end
       classifier.save_to_database!
+    end
+  end
+
+  def self.classify_by_mnb
+    classifiers_ensemble = ClassifiersEnsemble.new( [Classifier.find_by_name("#{Classifier::NAIVE_BAYES_NAME}-all")], :preload => true )
+
+    Feed.unclassified_fetched.each do |feed|
+      classify_info = classifiers_ensemble.classify( feed.string_for_classifier )
+      tag_list = ["classified"]
+      if classify_info[:recommend_as_train] == true
+        tag_list << "to_train"
+      end
+      feed.mark_list += tag_list
+      feed.text_class = TextClass.find classify_info[:class]
+      feed.save
     end
   end
 
