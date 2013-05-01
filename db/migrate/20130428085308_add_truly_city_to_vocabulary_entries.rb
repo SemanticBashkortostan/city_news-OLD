b91ea00492ee1e0800f572b0649137d7be1401d1
Idris <sld7700@gmail.com>
2013-05-01 16:20:18 +0600

    Updated truly_city migration - added false setting to exist VE's

--- db/migrate/20130428085308_add_truly_city_to_vocabulary_entries.rb
+++ db/migrate/20130428085308_add_truly_city_to_vocabulary_entries.rb
@@ -1,5 +1,9 @@
 class AddTrulyCityToVocabularyEntries < ActiveRecord::Migration
   def change
     add_column :vocabulary_entries, :truly_city, :boolean, :default => false
+    VocabularyEntry.all.each do |ve|
+      ve.truly_city ||= false
+      ve.save! if ve.changed?
+    end
   end
 end
