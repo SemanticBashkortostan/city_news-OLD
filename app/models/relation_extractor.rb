d83b3d880737895b5512e79c3fdea55bf282d46a
Idris <sld7700@gmail.com>
2013-05-03 17:43:31 +0600

    Refactored and updated models/relation_extractor.rb. Now feature_fetcher/relation_extractor.rb not needs.

--- app/models/relation_extractor.rb
+++ app/models/relation_extractor.rb
@@ -1,11 +1,15 @@
 #coding: utf-8
 
 class RelationExtractor
-  def initialize
-    load_test_and_training_sets
+  def initialize( need_preload = false )
+    preload if need_preload
+    @patterns_filename = "#{Rails.root}/classifiers/relation_extractor/patterns_hash"
+    @path = @patterns_filename.split("/")[0...-1].join("/")
+    FileUtils.mkdir_p(@path) unless File.exists?(path)
   end
 
 
+  # Generalize pattern from Feed#feature_vectors_for_relation_extraction
   def filter_features_in example, train=false
     ending_keys = [ :tc_right_context, :tc_left_context, :ne_right_context, :ne_left_context]
     other_keys = [ :tc_is_first_token, :has_other_cities, :in_one_sent,
@@ -26,6 +30,7 @@ class RelationExtractor
   end
 
 
+  # Get generalized patterns from set of feed.feature_vectors_for_relation_extraction
   def get_generalized_patterns set=nil
     dipre_hash = {}
     set ||= @positive_train_set
@@ -35,12 +40,70 @@ class RelationExtractor
       dipre_hash[example] = dipre_hash[example].to_i + 1
     end
 
-    patterns = Hash[dipre_hash.find_all{|k,v| v > 1}]
+    patterns = Hash[dipre_hash.find_all{|generalized_pattern, seen_count| seen_count > 1}]
     return patterns
   end
 
 
-  def test( patterns_hash = nil )
+  # Extract new dict from given set of feature vectors and patterns
+  def extract_new_dict( set, patterns_hash )
+    new_dict = {}
+    set.each do |(example, id)|
+      to_apply_example = example.clone
+      filter_features_in(to_apply_example)
+      new_dict[example[:text_class_id]] ||= Set.new
+      new_dict[example[:text_class_id]] += [example[:ne_stem], example[:tc_stem]] if patterns_hash[to_apply_example]
+    end
+    return new_dict
+  end
+
+
+  # 1. Find or set patterns from given feature vectors, 2. get new feature vectors from patterns,
+  # 3. return to 1 with new feature vectors
+  # It saves all patterns into @patterns_filename
+  def iteratively_extract_patterns( from_file = false, patterns_hash = nil )
+    from_file ? patterns_hash ||= FileMarshaling.marshal_load( @patterns_filename ) : patterns_hash ||= get_generalized_patterns( @positive_train_set )
+
+    loop do
+      init_count = patterns_hash.count
+
+      new_dict = extract_new_dict(@maybe_negative_set, patterns_hash)
+      new_vectors = extract_vectors_for_relation_extractor( new_dict )[0]
+
+      patterns_hash.merge!( get_generalized_patterns(new_vectors) )
+      puts "Old Count: #{init_count} New Count: #{patterns_hash.count}"
+
+      break if init_count == patterns_hash.count
+    end
+    performance(patterns_hash)
+    FileMarshaling.marshal_save @patterns_filename, patterns_hash
+    return patterns_hash
+  end
+
+
+  # Get new words and fill vocabulary using patterns_hash from @patterns_filename
+  # First you need to create patterns_hash. It can be done with +iteratively_extract_patterns+
+  def extract_new_words_and_fill_vocabulary!
+    patterns_hash = FileMarshaling.marshal_load @patterns_filename
+    new_dict = extract_new_dict( @maybe_negative_set, patterns_hash)
+    new_dict.each do |tc_id, tc_dict|
+      tc_dict.each do |word|
+        ve = VocabularyEntry.new :token => word, :state => VocabularyEntry::TESTING_STATE
+        ve.text_classes << TextClass.find(tc_id)
+        ve.save
+      end
+    end
+    puts "#{VocabularyEntry.accepted.only_tokens.count} / #{VocabularyEntry.unscoped.with_state(:testing).only_tokens.count}"
+  end
+
+
+  def preload
+    load_test_sets
+    load_train_sets
+  end
+
+
+  def performance( patterns_hash = nil )
     confusion_matrix = { 1 => { 1=>0, -1 => 0}, -1 => {1=>0, -1=>0} }
     patterns_hash ||= get_generalized_patterns
     @positive_test_set.each do |(example, id)|
@@ -64,103 +127,87 @@ class RelationExtractor
   end
 
 
-  def extract_new_dict( set, patterns_hash )
-    new_dict = {}
-    set.each do |(example, id)|
-      to_apply_example = example.clone
-      filter_features_in(to_apply_example)
-      new_dict[example[:text_class_id]] ||= Set.new
-      new_dict[example[:text_class_id]] += [example[:ne_stem], example[:tc_stem]] if patterns_hash[to_apply_example]
-    end
-    return new_dict
-  end
-
-
-  # Находим слова и ищем фиды с этими словами, потом опять генерелизуем паттерны, пока находятся слова.
-  def iteratively_extract_patterns
-    patterns_hash = get_generalized_patterns( @positive_train_set )
-    ff_relation_extractor = FeatureFetcher::RelationExtractor.new
-    ff_relation_extractor.preload_feeds_data
-
-    loop do
-      init_count = patterns_hash.count
-
-      new_dict = extract_new_dict(@maybe_negative_set, patterns_hash)
-      new_vectors = ff_relation_extractor.extract_vectors_for_relation_extractor( new_dict )
-
-      patterns_hash.merge!( get_generalized_patterns(new_vectors) )
-      puts "Old Count: #{init_count} New Count: #{patterns_hash.count}"
-
-      break if init_count == patterns_hash.count
-    end
-    test(patterns_hash)
-    save_set "patterns_hash", patterns_hash
-    return patterns_hash
-  end
-
-
-  # TODO: TEST WITH NEW DATA!!!
-  def dev_extract_new_words
-    patterns_hash = load_set("patterns_hash", true)
-    new_dict = extract_new_dict( @maybe_negative_set, patterns_hash)
-    new_dict.each do |tc_id, tc_dict|
-      tc_dict.each do |word|
-        ve = VocabularyEntry.new :token => word, :state => VocabularyEntry::TESTING_STATE
-        ve.text_classes << TextClass.find(tc_id)
-        ve.save
-      end
-    end
-    puts "#{VocabularyEntry.accepted.only_tokens.count} / #{VocabularyEntry.unscoped.with_state(:testing).only_tokens.count}"
-  end
-
-
   protected
 
 
-  def load_test_and_training_sets
-    load_test_sets
-    load_train_sets
+  def generate_dict_from_vocabulary_entry
+    dict = {}
+    TextClass.all.each do |id|
+      dict[id] = VocabularyEntry.for_city(id).collect(&:token).to_set
+    end
+    return dict
   end
 
 
+  def generate_truly_rules_from_vocabulary_entry
+    truly_rules = {}
+    TextClass.all.each do |id|
+      truly_rules[id] = Regexp.new(VocabularyEntry.make_regexp_for_truly_entries(id)[0])
+    end
+    return truly_rules
+  end
+
+
+  def get_feeds
+    Feed.cached
+  end
+
+
+  # Return [positive_set, maybe_negative_set]; Positive set forming by check( text class token is true text class) and (named entity is on dict)
+  def extract_vectors_for_relation_extractor(dict = nil)
+     positive_set = []
+     maybe_negative_set = []
+     dict ||= generate_dict_from_vocabulary_entry
+     truly_rules = generate_truly_rules_from_vocabulary_entry
+     feeds = get_feeds
+     feeds.each_with_index do |feed, ind|
+       p "proccesed #{feed.id} :: #{ind}/#{feeds.count}"
+       feed_feature_vectors = feed.feature_vectors_for_relation_extraction
+       next unless feed_feature_vectors
+       feed_feature_vectors.each { |fv|
+         inserted_to_positive  = false
+         TextClass.all.each do |tc|
+           tc_id = tc.id
+           if fv[:tc_stem] =~ truly_rules[tc_id] && fv[:tc_stem] != fv[:ne_stem] && fv[:ne_stem].length > 1
+             city_dictionary = dict[tc_id]
+             if city_dictionary.include?(fv[:ne_stem])
+               positive_set << [fv, feed.id]
+               inserted_to_positive = true
+               break
+             end
+           end
+         end
+         maybe_negative_set << [fv, feed.id] if !inserted_to_positive
+       }
+     end
+     return [positive_set, maybe_negative_set]
+   end
+
+
   def load_test_sets
-    test_positive_filename = "positive_re_test_set-new"
-    test_negative_filename = "negative_re_test_set-new"
-    @positive_test_set = load_set test_positive_filename
-    @negative_test_set = load_set test_negative_filename
+    test_positive_filename = "#{@path}/positive_re_test_set-new"
+    test_negative_filename = "#{@path}/negative_re_test_set-new"
+    @positive_test_set = FileMarshaling.marshal_load test_positive_filename
+    @negative_test_set = FileMarshaling.marshal_load test_negative_filename
   end
 
 
   def load_train_sets
-    positive_filename = "positive_re_set-new"
-    @positive_train_set = load_set positive_filename
-
-    negative_filename = "negative_re_set-new"
-    @maybe_negative_set = load_set negative_filename
+    pos_and_neg_sets = extract_vectors_for_relation_extractor
+    @positive_train_set = pos_and_neg_sets[0]
+    @maybe_negative_set = pos_and_neg_sets[1]
   end
 
-
-  def load_set filename, only_load=false
-    loaded_hash = Marshal.load(File.binread(filename))
-    return loaded_hash if only_load || !loaded_hash.is_a?( Hash )
-    return loaded_hash.values.to_a.flatten if loaded_hash.is_a?( Hash )
-  end
-
-
-  def save_set filename, set
-    File.open(filename,'wb') do |f|
-      f.write Marshal.dump(set)
-    end
-  end
 
 
   # count=51
   # Only Named Entities
+  # Interactively creating test sets( needs only for development )
   def form_sets
-    positive_filename = "positive_re_set-new"
+    positive_filename = "#{@path}/positive_re_set-new"
     positive_set = load_set positive_filename
 
-    negative_filename = "negative_re_set-new"
+    negative_filename = "#{@path}/negative_re_set-new"
     negative_set = load_set negative_filename
 
     positive_test_set = []
