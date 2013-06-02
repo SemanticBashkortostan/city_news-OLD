f0657a7f9951e2c48c79ddb79034a0f93a326e3f
Idris <sld7700@gmail.com>
2013-06-03 00:44:51 +0600

    Deleted old DocsCount because now docs_count is store in Classifier#parameters:hstore

--- lib/clock.rb
+++ lib/clock.rb
@@ -30,10 +30,7 @@ every(3.minutes, 'production_feeds:fetch_and_classify') do
   Scheduler::ProductionFeedsFetcher.new.fetch_and_classify
 end
 
-#every(11.minutes, 'fetch_other_feeds') do
-#   feeds_fetcher = Scheduler::ProductionFeedsFetcher.new
-#   feeds_fetcher.fetch_outlier_feeds
-#end
+
 #
 #every(31.minutes, 'classifier:train_by_production_data') do
 #  #Scheduler::Classifier.train_by_production_data
