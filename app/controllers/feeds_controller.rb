c4a1070826d942577173bd20fc9cf7f2aa08edab
Idris <sld7700@gmail.com>
2013-10-12 00:26:09 +0400

    Some refactor in FeedsController. Now only .root feeds fetch there

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -24,21 +24,27 @@ class FeedsController < ApplicationController
 
 
   def index
+    scope = Feed
     if params[:city] && params[:city] != "all"
       text_class = TextClass.find_by_eng_name( params[:city].capitalize )
       @title = text_class.name if params[:city]
       @description = "Новости #{text_class.prepositional_name}."
-      @feeds = Feed.with_text_klass( text_class.id ).order 'published_at desc'
+      scope = scope.with_text_klass( text_class.id ).order 'published_at desc'
     else
-      @feeds = Feed.includes(:text_class).where('text_class_id is not null').order 'published_at desc'
+      scope = scope.includes(:text_class).where('text_class_id is not null').order 'published_at desc'
     end
-    @feeds = @feeds.page params[:page]
+    @feeds = scope.roots.page params[:page]
     @grouped_feeds = @feeds.where('published_at is not null').group_by{ |feed| feed.published_at.strftime("%d-%m-%Y") }
     respond_to do |format|
       format.html
       format.json{ render :json => @feeds }
       format.xml{ render :xml => @feeds }
     end
+  end
+
+
+  def show
+    @feed = Feed.find params[:id]
   end
 
 
