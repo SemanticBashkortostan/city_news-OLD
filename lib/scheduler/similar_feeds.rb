31446b0c57dc51cd5e33cfa59971e246586ae805
Idris <sld7700@gmail.com>
2013-10-14 22:21:30 +0400

    Fixed Scheduler::SimilarFeeds

--- lib/scheduler/similar_feeds.rb
+++ lib/scheduler/similar_feeds.rb
@@ -52,23 +52,27 @@ class Scheduler::SimilarFeeds
 
 
   def perform_last_feeds last_feeds = nil
-    last_feeds ||= Feed.where('published_at > ?', 10.minutes.ago).
-        where(text_class_id: @text_classes, ancestry: nil).order('published_at desc').all
-    compare_feeds = Feed.where(text_class_id: @text_classes).
-          where('published_at > ?', 2.days.ago).order('published_at desc').all
+    last_feeds_scope ||= Feed.where('published_at > ?', 30.minutes.ago).
+        where(text_class_id: @text_classes, ancestry: nil).order('published_at desc')
+    compare_feeds_scope = Feed.where(text_class_id: @text_classes).
+          where('published_at > ?', 2.days.ago).order('published_at desc')
 
     ActiveRecord::Base.transaction do
-      for i in 0...last_feeds.count
-        for j in i+1...compare_feeds.count
-          next if !compare_feeds[j] || last_feeds[i].id == compare_feeds[j].id
+      @text_classes.each do |tc|
+        last_feeds = last_feeds_scope.where(:text_class_id => tc.id)
+        compare_feeds = compare_feeds_scope.where(:text_class_id => tc.id)
+        for i in 0...last_feeds.count
+          for j in i+1...compare_feeds.count
+            next if !compare_feeds[j] || last_feeds[i].id == compare_feeds[j].id
 
-          similar_score = last_feeds[i].similarity(compare_feeds[j])
-          if similar_score > 0.84
-            set_children_to_feed(last_feeds[i], compare_feeds[j], similar_score, false)
-          elsif similar_score >= 0.7
-            set_children_to_feed(last_feeds[i], compare_feeds[j], similar_score, true)
+            similar_score = last_feeds[i].similarity(compare_feeds[j])
+            if similar_score > 0.84
+              set_children_to_feed(last_feeds[i], compare_feeds[j], similar_score, false)
+            elsif similar_score >= 0.7
+              set_children_to_feed(last_feeds[i], compare_feeds[j], similar_score, true)
+            end
+
           end
-
         end
       end
     end
