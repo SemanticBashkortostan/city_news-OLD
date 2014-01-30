2ec0fbd1724f4651203f276ce2b6b9933e01de5c
Idris <sld7700@gmail.com>
2014-01-31 00:34:09 +0400

    Reverted position of google analytics script place

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -27,11 +27,4 @@ jQuery(function($){
             $(this).removeClass("loading");
         }
     });
-
-    var _gaq = _gaq || [];
-    _gaq.push(['_setAccount', 'UA-38023734-1']);
-    _gaq.push(['_trackPageview']);
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
 });
